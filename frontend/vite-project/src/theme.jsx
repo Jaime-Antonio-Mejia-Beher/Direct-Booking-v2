2e293fcbb9e8fa4f6e06da95f4d42be2b482dc9e
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-31 10:06:36 -0600

    modified index css, deleted app css, updated theme

--- frontend/vite-project/src/theme.jsx
+++ frontend/vite-project/src/theme.jsx
@@ -2,12 +2,20 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
+    mode: "light",
     primary: {
       main: "#007bff",
     },
     secondary: {
       main: "#ff4081",
     },
+    background: {
+      default: "#ffffff",
+      paper: "#f5f5f5",
+    },
+  },
+  typography: {
+    fontFamily: "Inter, system-ui, Avenir, Helvetica, Arial, sans-serif",
   },
 });
 
