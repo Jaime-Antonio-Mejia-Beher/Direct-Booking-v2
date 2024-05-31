dda100c576656ed8c921a228e1212f2bff1059ef
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-31 15:53:52 -0600

    added css to <Box> component and updated accommodationsData with more accurate description

--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import {
   Container,
   Box,
   Typography,
+  Grid,
   Card,
   CardMedia,
   CardContent,
@@ -19,35 +20,41 @@ const Home = () => {
         <Typography variant="h4" gutterBottom>
           OUR ACCOMMODATION OPTIONS
         </Typography>
-        {accommodations.map((accommodation) => (
-          <Card key={accommodation.id} sx={{ my: 2 }}>
-            <CardMedia
-              component="img"
-              alt={accommodation.description}
-              height="300"
-              image={accommodation.img}
-              title={accommodation.description}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {accommodation.city}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {accommodation.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                size="small"
-                color="primary"
-                component={Link}
-                to={accommodation.link}
-              >
-                View Details
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
+        <Grid container spacing={4}>
+          {accommodations.map((accommodation) => (
+            <Grid item xs={12} sm={6} md={4} key={accommodation.id}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  alt={accommodation.description}
+                  height="300"
+                  image={accommodation.img}
+                  title={accommodation.description}
+                />
+                <CardContent sx={{height: "80px"}}>
+                  <Typography variant="body1" color="text.secondary">
+                    {accommodation.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', padding: '0px 10px'}} >
+                  <Typography gutterBottom variant="h5" component="div">
+                    {accommodation.city}
+                  </Typography>
+                  <Button
+                    size="small"
+                    color="primary"
+                    component={Link}
+                    to={accommodation.link}
+                    >
+                    View Details
+                  </Button>
+                    </Box>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Container>
   );
