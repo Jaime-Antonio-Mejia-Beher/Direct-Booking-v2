2e42d4303890a8d6f829a973cfadfb1b2609123c
Jaime Mejia-Beher <jmejiabeher@gmail.com>
2024-05-31 12:07:40 -0600

    eliminated title from accommodations array

--- frontend/vite-project/src/pages/Home.jsx
+++ frontend/vite-project/src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Accommodation from "../components/Accommodation";
 
 const accommodations = [
   {
-    title: "1 bedroom 1 bath",
     description: "Ping-Pong | King Bed | DIA Airport  Kitchenette | Private | Denver, CO",
     img: "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6NTAxNTcyNzk%3D/original/38fd4c60-4fc6-4c38-af85-2be319f100df.png?im_w=960",
     link: "",
@@ -12,7 +11,6 @@ const accommodations = [
     city: "Denver, CO",
   },
   {
-    title: "4 bedroom 3 bath",
     description: "Low Cleaning Fee | DIA Airport | Patio  Denver, CO",
     img: "https://a0.muscache.com/im/pictures/miso/Hosting-694925671710075642/original/572adddb-a2c1-4901-81e4-13c64f7cbe86.png?im_w=320",
     link: "https://a0.muscache.com/im/pictures/miso/Hosting-694925671710075642/original/572adddb-a2c1-4901-81e4-13c64f7cbe86.png?im_w=320",
@@ -20,7 +18,6 @@ const accommodations = [
     city: "Cupertino, CA",
   },
   {
-    title: "1 bedroom 1 bath",
     description: "Private Unit | Full Kitchen | King Bed  Cupertino, CA ",
     img: "https://a0.muscache.com/im/pictures/miso/Hosting-29826475/original/4d93439c-5d4a-42ef-ae4f-66eaab75ac04.png?im_w=960",
     link: "",
@@ -40,7 +37,6 @@ const Home = () => {
           {accommodations.map((accommodation) => (
             <Accommodation
               key={accommodation.title}
-              title={accommodation.title}
               description={accommodation.description}
               img={accommodation.img}
               link={accommodation.link}
